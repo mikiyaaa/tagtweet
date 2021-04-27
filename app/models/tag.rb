32053c79c972419215e025a012a4b96531aa1200
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-04-27 22:42:56 +0900

    タグ付機能実装

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
   has_many :tweet_tag_relations
   has_many :tweets, through: :tweet_tag_relations
+
+  # 一意性制約はモデル単位で設ける必要がある
+  validates :name, uniqueness: true
 end
